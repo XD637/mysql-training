1e1b9a9d01f234c1fdd88618a62a926e14918373
XD637 <rithveya@gmail.com>
2025-04-10 15:24:58 +0530

    update WebSocket server to support username-based group chat functionality

--- websocket.js
+++ websocket.js
@@ -1,32 +1,41 @@
 import { WebSocketServer } from "ws";
+import url from "url"; 
 
 const wss = new WebSocketServer({ port: 8080 });
 
 console.log("WebSocket server running on ws://localhost:8080");
 
 
-const clients = new Set();
+const clients = new Map();
 
-wss.on("connection", (ws) => {
-    
-    clients.add(ws);
-    console.log("A client connected");
+wss.on("connection", (ws, req) => {
+    const query = url.parse(req.url, true).query;
+    const username = query.username || "Anonymous";
+    clients.set(ws, username);
+    console.log(`${username} Connected`);
 
 
-    ws.send("Welcome to the group chat!");
+    ws.send(`Welcome to the group chat ${username} !`);
 
     ws.on("message", (message) => {
-        console.log(`Message received: ${message}`);
+        // if (message === "typing"){
+        //     for (const client of clients){
+        //         if(client !== ws && client.readyState === WebSocket.OPEN){
+        //             client.send(`${username} is typing...`);
+        //         };
+        //     };
+        // };
+        console.log(`Message received from ${username}: ${message}`);
 
         for (const client of clients) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(`Someone says: ${message}`);
+                client.send(`${username} says: ${message}`);
             }
         }
     });
 
     ws.on("close", () => {
-        console.log("A client disconnected");
+        console.log(`${username} Disconnected`);
         clients.delete(ws);
     });
 });
